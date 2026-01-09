2bac3f5d6758ac6b64297f05fa92867da376f7f1
Ellis Breen <1263585+griels@users.noreply.github.com>
2026-01-09 15:40:41 +0000

    Update SHA256 checksum for u-he Satin cask

--- Casks/u-he-satin.rb
+++ Casks/u-he-satin.rb
@@ -4,7 +4,7 @@ base_dir = ENV.fetch('HOMEBREW_CASK_MUSIC_SOFTWARE_BASEDIR', Dir.pwd)
 
 cask 'u-he-satin' do
   version '1.3.1,7414'
-  sha256 '054a168daf4f4e8a85e2bf7bc374dcfda861d1e677e9b9f054326fe43ce0d3f5'
+  sha256 '324e2f68168aed4341bd333fade54a23888d621e7406039c55d7596fa7f2a8b3'
 
   # :base_dir was verified as official when first introduced to the cask
   url Addressable::URI.encode("https://dl.u-he.com/releases/Satin_133_15721_Mac.zip")
